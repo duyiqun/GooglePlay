b6946218378ba41e34c5207263ecd4c40b1034df
Qun <slvt@qq.com>
2017-05-02 13:08:47 +0800

    完成ActionBar的初始化

--- app/src/main/java/com/qun/googleplay/MainActivity.java
+++ app/src/main/java/com/qun/googleplay/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.activity_root)
     DrawerLayout mActivityRoot;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +36,27 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayShowHomeEnabled(true);
 
         //箭头对象
-        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(MainActivity.this, mActivityRoot, R.string.open, R.string.close);
+        mDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, mActivityRoot, R.string.open, R.string.close);
 
+        //同步
+        mDrawerToggle.syncState();
+
+        //设置监听
+        mActivityRoot.addDrawerListener(mDrawerToggle);
+    }
+
+    //菜单点击事件
+    //记住select
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home://箭头对象
+                mDrawerToggle.onOptionsItemSelected(item);
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //初始化代码
