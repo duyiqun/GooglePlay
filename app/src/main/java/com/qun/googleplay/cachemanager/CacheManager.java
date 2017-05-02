02f88f922412fcb6c10a47d7c0c0045e4f506026
Qun <slvt@qq.com>
2017-05-02 19:23:20 +0800

    缓存保存数据

--- app/src/main/java/com/qun/googleplay/cachemanager/CacheManager.java
+++ app/src/main/java/com/qun/googleplay/cachemanager/CacheManager.java
@@ -1,5 +1,12 @@
 package com.qun.googleplay.cachemanager;
 
+import android.os.Environment;
+
+import com.qun.googleplay.global.GooglePlay;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * 缓存管理类
  * Created by Qun on 2017/5/2.
@@ -24,7 +31,28 @@ class CacheManager {
 
     //保存对应的数据
     public void saveCacheData(String url, String content) {
+        //sd卡根目录包名
+        try {
+            String path = Environment.getExternalStorageDirectory().getPath() + File.separator + GooglePlay.context.getPackageName();
+            //文件夹多级目录应该手动创建
+            File pathDir = new File(path);
 
+            //多级目录需要创建
+            if (!pathDir.exists()) {
+                pathDir.mkdirs();
+            }
 
+            File file = new File(pathDir, getFileName(url));
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(content.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getFileName(String url) {
+        return "test";
     }
 }
--- app/src/main/java/com/qun/googleplay/cachemanager/NewManager.java
+++ app/src/main/java/com/qun/googleplay/cachemanager/NewManager.java
@@ -1,5 +1,11 @@
 package com.qun.googleplay.cachemanager;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * 统一网络管理类
  * Created by Qun on 2017/5/2.
@@ -18,7 +24,14 @@ public class NewManager {
 
     //根据网络地址返回数据
     public String dataGet(String url) {
-
-        return "";
+        try {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+            Request request = new Request.Builder().url(url).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;//处理异常
+        }
     }
 }
