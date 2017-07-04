c80b5d801e659b1e2040f3d8d0e096bb93e26f75
y480-pc\Qun <slvt@qq.com>
2017-07-04 21:44:09 +0800

    展示home列表

---
+++ app/src/main/java/com/qun/googleplay/adapter/HomeListAdapter.java
@@ -0,0 +1,86 @@
+package com.qun.googleplay.adapter;
+
+import android.text.format.Formatter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.qun.googleplay.R;
+import com.qun.googleplay.bean.HomeBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Qun on 2017/7/4.
+ */
+
+public class HomeListAdapter extends BaseAdapter {
+
+    private List<HomeBean.HomeItem> mShowItems = new ArrayList();
+
+    public HomeListAdapter(List<HomeBean.HomeItem> showItems) {
+        mShowItems = showItems;
+    }
+
+    @Override
+    public int getCount() {
+        return mShowItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mShowItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = View.inflate(parent.getContext(), R.layout.item_home, null);
+            viewHolder = new ViewHolder(convertView);
+//            viewHolder = new ViewHolder();
+//            viewHolder.ivHomeIcon = (ImageView) convertView.findViewById(R.id.iv_home_icon);
+//            viewHolder.tvHomeTitle = (TextView) convertView.findViewById(R.id.tv_home_title);
+//            viewHolder.rbHomeStart = (RatingBar) convertView.findViewById(R.id.rb_home_start);
+//            viewHolder.tvHomeSize = (TextView) convertView.findViewById(R.id.tv_home_size);
+//            viewHolder.tvHomeDesc = (TextView) convertView.findViewById(R.id.tv_home_desc);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        //数据绑定(更新)
+        viewHolder.tvHomeTitle.setText(mShowItems.get(position).getName());
+        viewHolder.rbHomeStart.setRating(mShowItems.get(position).getStars());
+        //格式大小
+        String fileSize = Formatter.formatFileSize(parent.getContext(), mShowItems.get(position).getSize());
+        viewHolder.tvHomeSize.setText(fileSize);
+        viewHolder.tvHomeDesc.setText(mShowItems.get(position).getDes());
+        return convertView;
+    }
+
+    class ViewHolder {
+
+        ImageView ivHomeIcon;
+        TextView tvHomeTitle;
+        RatingBar rbHomeStart;
+        TextView tvHomeSize;
+        TextView tvHomeDesc;
+
+        public ViewHolder(View view) {
+            ivHomeIcon = (ImageView) view.findViewById(R.id.iv_home_icon);
+            tvHomeTitle = (TextView) view.findViewById(R.id.tv_home_title);
+            rbHomeStart = (RatingBar) view.findViewById(R.id.rb_home_start);
+            tvHomeSize = (TextView) view.findViewById(R.id.tv_home_size);
+            tvHomeDesc = (TextView) view.findViewById(R.id.tv_home_desc);
+        }
+    }
+}
--- app/src/main/java/com/qun/googleplay/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/qun/googleplay/ui/fragment/HomeFragment.java
@@ -1,11 +1,20 @@
 package com.qun.googleplay.ui.fragment;
 
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.qun.googleplay.R;
+import com.qun.googleplay.adapter.HomeListAdapter;
 import com.qun.googleplay.bean.HomeBean;
 import com.qun.googleplay.cachemanager.JsonCacheManager;
 import com.qun.googleplay.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Qun on 2017/5/2.
@@ -13,14 +22,25 @@ import com.qun.googleplay.utils.Utils;
 
 public class HomeFragment extends BaseFragment {
 
-    private TextView mTextView;
+    @BindView(R.id.lv_home_list_layout)
+    ListView mLvHomeListLayout;
+    Unbinder unbinder;
+    private List<HomeBean.HomeItem> mShowItems = new ArrayList<>();
+    private HomeListAdapter mHomeListAdapter;
 
     //创建一个界面
     @Override
     protected View createView() {
-        mTextView = new TextView(getContext());
-        mTextView.setText("我是框架生的");
-        return mTextView;
+        View view = View.inflate(getContext(), R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
+        init();
+        return view;
+    }
+
+    //初始化代码
+    private void init() {
+        mHomeListAdapter = new HomeListAdapter(mShowItems);
+        mLvHomeListLayout.setAdapter(mHomeListAdapter);
     }
 
     //给个数据
@@ -29,14 +49,22 @@ public class HomeFragment extends BaseFragment {
         //获取数据
         final HomeBean homeBean = (HomeBean) JsonCacheManager.getInstance().getDataBean("http://127.0.0.1:8090/home?index=0", HomeBean.class);
 
+        mShowItems.addAll(homeBean.getList());
+
         //更新数据
         Utils.runOnUIThread(new Runnable() {
             @Override
             public void run() {
-                mTextView.setText(homeBean.getList().get(0).getName());
+                mHomeListAdapter.notifyDataSetChanged();
             }
         });
 
         return "";
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
