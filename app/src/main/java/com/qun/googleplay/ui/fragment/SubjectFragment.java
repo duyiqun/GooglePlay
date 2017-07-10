10beeba5887bad72c3bdb6583014c4e5f54478ec
y480-pc\Qun <slvt@qq.com>
2017-07-10 23:56:51 +0800

    完成专题逻辑

--- app/src/main/java/com/qun/googleplay/ui/fragment/SubjectFragment.java
+++ app/src/main/java/com/qun/googleplay/ui/fragment/SubjectFragment.java
@@ -3,10 +3,13 @@ package com.qun.googleplay.ui.fragment;
 import android.view.View;
 import android.widget.ListView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.qun.googleplay.R;
 import com.qun.googleplay.adapter.SubjectListAdapter;
 import com.qun.googleplay.bean.SubjectBean;
 import com.qun.googleplay.cachemanager.JsonCacheManager;
+import com.qun.googleplay.utils.ToastUtil;
 import com.qun.googleplay.utils.Uris;
 import com.qun.googleplay.utils.Utils;
 
@@ -23,9 +26,9 @@ import butterknife.Unbinder;
 
 public class SubjectFragment extends BaseFragment {
 
-    @BindView(R.id.lv_subject_list_layout)
-    ListView mLvSubjectListLayout;
     Unbinder unbinder;
+    @BindView(R.id.pull_refresh_list)
+    PullToRefreshListView mPullRefreshList;
 
     private List<SubjectBean> mShowItems = new ArrayList<>();
     private SubjectListAdapter mSubjectListAdapter;
@@ -41,23 +44,68 @@ public class SubjectFragment extends BaseFragment {
 
     private void init() {
         mSubjectListAdapter = new SubjectListAdapter(mShowItems);
-        mLvSubjectListLayout.setAdapter(mSubjectListAdapter);
+        ListView refreshableView = mPullRefreshList.getRefreshableView();
+        refreshableView.setAdapter(mSubjectListAdapter);
+
+        //设置模式
+        mPullRefreshList.setMode(PullToRefreshBase.Mode.BOTH);
+
+        //监听
+        mPullRefreshList.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                //重新请求数据
+                mLoadPager.showPager();
+            }
+        });
     }
 
     @Override
     public Object getData() {
+        //判断当前的模式
+        if (mPullRefreshList.getCurrentMode() == PullToRefreshBase.Mode.PULL_FROM_START) {
+            //清空数据
+            mShowItems.clear();
+        }
+
         //请求数据
-        List<SubjectBean> subjectBeanList = JsonCacheManager.getInstance().getDataList(Uris.SUBJECT_ADDRESS, SubjectBean.class);
-        mShowItems.addAll(subjectBeanList);
+        List<SubjectBean> subjectBeanList = JsonCacheManager.getInstance().getDataList(Uris.SUBJECT_ADDRESS + mShowItems.size(), SubjectBean.class);
+
+        /**
+         * 1.当前的总集合等于0，当前的集合为0 失败
+         * 2.当前的总集合等于0，当前的集合大于0，添加
+         * 3.当前的总集合大于0，当前集合为0，没有数据
+         * 4.当前的总集合大于0，当前集合大于0，添加
+         */
+
+        if (mShowItems.size() == 0) {
+            if (subjectBeanList == null || subjectBeanList.size() == 0) {
+                //1.当前的总集合等于0，当前的集合为0 失败
+                return null;
+            }else {
+                //2.当前的总集合等于0，当前的集合大于0，添加
+                mShowItems.addAll(subjectBeanList);
+            }
+        } else {
+            if (subjectBeanList == null || subjectBeanList.size() == 0) {
+                //3.当前的总集合大于0，当前集合为0，没有数据
+                ToastUtil.showToast("当前已无数据");
+            }else {
+                //4.当前的总集合大于0，当前集合大于0，添加
+                mShowItems.addAll(subjectBeanList);
+            }
+        }
 
         //更新ui
         Utils.runOnUIThread(new Runnable() {
             @Override
             public void run() {
                 mSubjectListAdapter.notifyDataSetChanged();
+                //刷新的状态置空
+                mPullRefreshList.onRefreshComplete();
             }
         });
-        return subjectBeanList;
+        return mShowItems;
     }
 
     @Override
