88a1adfcb8f20088c96096c67514c983c3fa94ae
y480-pc\Qun <slvt@qq.com>
2017-07-16 23:26:18 +0800

    优化线程池的创建

--- app/src/main/java/com/qun/googleplay/downmanager/ThreadPoolManager.java
+++ app/src/main/java/com/qun/googleplay/downmanager/ThreadPoolManager.java
@@ -1,5 +1,7 @@
 package com.qun.googleplay.downmanager;
 
+import android.os.AsyncTask;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -17,10 +19,16 @@ public class ThreadPoolManager {
 
         if (mThreadPoolExecutor == null) {
             //线程池创建
+            int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+            // We want at least 2 threads and at most 4 threads in the core pool,
+            // preferring to have 1 less than the CPU count to avoid saturating
+            // the CPU with background work
+            int CORE_POOL_SIZE = Math.max(2, Math.min(CPU_COUNT - 1, 4));
+
             int coreSize = 3;//核心线程数，线程池一创建开启多少线程
             int maxSize = 10;//最大线程数，最大同时可以运行多少个
             long keepTime = 5;//存活时间
-            LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>(10);//队列
+            LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>(Integer.MAX_VALUE);//队列
             //new ThreadPoolExecutor.AbortPolicy()这个线程池的策略
             mThreadPoolExecutor = new ThreadPoolExecutor(coreSize, maxSize, keepTime, TimeUnit.MINUTES, blockingQueue, Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
@@ -30,6 +38,8 @@ public class ThreadPoolManager {
              * 3.判断最大线程数是否满，如果没满，直接开启
              * 4.如果队列与最大的线程数都满了，就去查看线程池策略
              */
+
+            
         }
     }
 
